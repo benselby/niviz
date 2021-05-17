fc81fb2e78764f03ea2514a644c20a37dfb5e934
benselby <Ben.Selby@camh.ca>
2021-05-17 13:10:34 -0400

    correcting formatting with yapf

--- niviz/interfaces/views.py
+++ niviz/interfaces/views.py
@@ -23,8 +23,6 @@ import numpy as np
 import trimesh
 
 from ..node_factory import register_interface
-
-
 """
 ReportCapable concrete classes for generating reports as side-effects
 """
@@ -141,7 +139,7 @@ class _ISegInputSpecRPT(nrc._SVGReportCapableInputSpec):
                      resolve=True,
                      desc='ROI Mask for mosaic',
                      mandatory=False)
-    
+
     masked = traits.Bool(False,
                          usedefault=True,
                          desc='Flag to indicate whether'
@@ -315,6 +313,7 @@ def _run_imports() -> None:
     register_interface(IFSCoregRPT, 'freesurfer_coreg')
     register_interface(ISurfVolRPT, 'surface_coreg')
 
+
 class _ISurfVolInputSpecRPT(nrc._SVGReportCapableInputSpec):
     '''
     Input spec for reports coregistering surface and volume images
@@ -329,8 +328,7 @@ class _ISurfVolInputSpecRPT(nrc._SVGReportCapableInputSpec):
     fg_nii = File(exists=True,
                   usedefault=False,
                   resolve=True,
-                  desc='Foreground NIFTI for SVG'
-                  )
+                  desc='Foreground NIFTI for SVG')
 
     surf_l = File(exists=True,
                   usedefault=False,
@@ -359,8 +357,8 @@ def gifti_get_mesh(gifti):
     Arguments:
         gifti (GiftiImage): Input GiftiImage
     '''
-    
-    v, t = gifti.agg_data(('pointset','triangle'))
+
+    v, t = gifti.agg_data(('pointset', 'triangle'))
     return v.copy(), t.copy()
 
 
@@ -375,13 +373,13 @@ def gifti_get_full_brain(l, r):
     '''
     l_vert, l_trig = gifti_get_mesh(l)
     r_vert, r_trig = gifti_get_mesh(r)
-    
+
     offset = l_trig.max() + 1
     r_trig += offset
-    
+
     verts = np.vstack((l_vert, r_vert))
     trigs = np.vstack((l_trig, r_trig))
-    
+
     return (verts, trigs, offset)
 
 
@@ -401,11 +399,11 @@ class ISurfVolRPT(SurfVolRC):
 
     def _post_run_hook(self, runtime):
         outputs = self.aggregate_outputs(runtime=runtime)
-        
+
         self._bg_nii = self.inputs.bg_nii
         self._fg_nii = self.inputs.fg_nii or None
-        self._surf_l = self.inputs.surf_l       
-        self._surf_r = self.inputs.surf_r       
+        self._surf_l = self.inputs.surf_l
+        self._surf_r = self.inputs.surf_r
         self._ncuts = self.inputs.n_cuts or 7
 
         # Propogate to superclass
@@ -413,27 +411,26 @@ class ISurfVolRPT(SurfVolRC):
 
     def _run_interface(self, runtime):
         return runtime
- 
+
     def _generate_report(self):
         '''Make a composite for co-registration of surface and volume images'''
-        
+
         l_surf = nib.load(self._surf_l)
         r_surf = nib.load(self._surf_r)
         vol_img = nib.load(self._bg_nii)
 
-        if vol_img.ndim==4:
-            vol_img = vol_img.slicer[:,:,:,0]
-        
+        if vol_img.ndim == 4:
+            vol_img = vol_img.slicer[:, :, :, 0]
+
         verts, trigs, offset = gifti_get_full_brain(l_surf, r_surf)
-        
+
         mesh = trimesh.Trimesh(vertices=verts, faces=trigs)
         mask_nii = nilearn.image.threshold_img(vol_img, 1e-3)
         cuts = cuts_from_bbox(mask_nii, cuts=self._ncuts)
 
-        sections = mesh.section_multiplane(
-                        plane_normal=[0,0,1],
-                        plane_origin=[0,0,0],
-                        heights=cuts['z'])
+        sections = mesh.section_multiplane(plane_normal=[0, 0, 1],
+                                           plane_origin=[0, 0, 0],
+                                           heights=cuts['z'])
 
         zh = nplot.plot_anat(vol_img, display_mode='z', cut_coords=cuts['z'])
 
@@ -441,23 +438,23 @@ class ISurfVolRPT(SurfVolRC):
             ax = zh.axes[z].ax
             if s:
                 for segs in s.discrete:
-                    ax.plot(*segs.T, color='r', linewidth=0.5) 
-        
+                    ax.plot(*segs.T, color='r', linewidth=0.5)
+
         if self._fg_nii:
-            fg_img = nib.load(self._fg_nii).slicer[:,:,:,0]
-            fg_img = nilearn.image.resample_to_img(fg_img, vol_img,
-                    interpolation="linear") 
+            fg_img = nib.load(self._fg_nii).slicer[:, :, :, 0]
+            fg_img = nilearn.image.resample_to_img(fg_img,
+                                                   vol_img,
+                                                   interpolation="linear")
             # Custom colormap with transparencies
             ncolors = 256
             basecmap = 'viridis_r'
             color_array = plt.get_cmap(basecmap)(range(ncolors))
             color_array[:, -1] = np.linspace(1.0, 0.0, ncolors)
-            
+
             # Set background intensity=0 to transparent
             color_array[0, :] = 0
             cmapviridis = mcolors.LinearSegmentedColormap.from_list(
-                basecmap,colors=color_array
-            )
+                basecmap, colors=color_array)
 
             zh.add_overlay(fg_img, cmap=cmapviridis)
 
